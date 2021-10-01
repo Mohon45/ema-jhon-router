6c1c1447eeb5196806ad1d038258236dd2f49dee
Mohon45 <mdmohon6145@gmail.com>
2021-10-02 01:25:28 +0600

    plase order and some improve

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -3,16 +3,28 @@ import useProducts from '../../Hooks/useProducts';
 import useCart from '../../Hooks/useCart'
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { useHistory } from 'react-router';
 
 const OrderReview = () => {
     const [products] = useProducts();
     const [cart, setCart] = useCart(products);
+    const history = useHistory();
+
+
+
     const handleRemove = key => {
         const newCart = cart.filter(product => product.key !== key);
         setCart(newCart);
         removeFromDb(key);
     }
+
+    const handlePlaceOrder = () => {
+        history.push('/placeorder');
+        setCart([]);
+        clearTheCart();
+    }
+
     return (
         <div className="shop-container">
             <div className="product-container">
@@ -25,7 +37,11 @@ const OrderReview = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    <button
+                    onClick={handlePlaceOrder} 
+                    className="btn-regular">Place Order</button>
+                </Cart>
             </div>
 
         </div>
